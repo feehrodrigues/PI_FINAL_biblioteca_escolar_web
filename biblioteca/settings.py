343479d1907881bbed9f430002ec42ac5835583a
feehrodrigues <feehrodriguesoficial@gmail.com>
2026-05-18 10:40:39 -0300

    Preparando para o Vercel

--- biblioteca/settings.py
+++ biblioteca/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-e)3**_2b%g!syuxe^u*lb3h4=0b+mh2p0sw43c4gqgwhpgm903
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
@@ -42,6 +42,7 @@ INSTALLED_APPS = [
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
@@ -116,7 +117,8 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/5.2/howto/static-files/
 
 STATIC_URL = 'static/'
-
+import os
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
 
